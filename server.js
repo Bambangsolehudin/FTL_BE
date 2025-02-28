7e6b04e6d8f4d53fb46085106c6b223d1de9e409
Bambang Solhud <42888942+Bambangsolehudin@users.noreply.github.com>
2025-02-28 18:29:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,32 @@ app.get('/api/meetings', (req, res) => {
 });
 
 // Endpoint untuk menambahkan ruang meeting baru
-app.post('/api/meetings', (req, res) => {
-  const newMeeting = { id: meetings.length + 1, ...req.body };
-  meetings.push(newMeeting);
-  res.status(201).json(newMeeting);
-});
+app.post(
+  '/meetings',
+  [
+    body('unit').notEmpty().withMessage('Unit harus diisi'),
+    body('ruangMeeting').notEmpty().withMessage('Ruang Meeting harus diisi'),
+    body('tanggalRapat').notEmpty().withMessage('Tanggal Rapat harus diisi'),
+    body('waktuMulai').isIn(waktuOptions).withMessage('Waktu Mulai tidak valid'),
+    body('waktuSelesai').isIn(waktuOptions).withMessage('Waktu Selesai tidak valid'),
+    body('jumlahPeserta').isInt({ min: 1 }).withMessage('Jumlah Peserta harus angka positif'),
+    body('jenisKonsumsi.snackSiang').isBoolean().withMessage('Jenis Konsumsi harus boolean'),
+    body('jenisKonsumsi.makanSiang').isBoolean().withMessage('Jenis Konsumsi harus boolean'),
+    body('jenisKonsumsi.snackSore').isBoolean().withMessage('Jenis Konsumsi harus boolean'),
+    body('nominalKonsumsi.snackSore').isBoolean().withMessage('Nominal Konsumsi harus boolean'),
+
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const newMeeting = req.body;
+    meetings.push(newMeeting);
+    res.status(201).json(newMeeting);
+  }
+);
 
 // Endpoint untuk menghapus ruang meeting berdasarkan ID
 app.delete('/api/meetings/:id', (req, res) => {
